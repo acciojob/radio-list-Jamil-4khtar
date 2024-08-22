80a3fa00848daf292021a9c8bb7e47c348dfb2af
Jamil-4khtar <156568813+Jamil-4khtar@users.noreply.github.com>
2024-08-22 16:45:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+var radios = document.getElementsByClassName('choose');
+
+for (let i = 0; i < radios.length; i++) {
+	if (i%2==0) {
+		var label = document.querySelector(`label[for='${radios[i].id}']`);
+		label.style.color = "red";
+	}
+}
